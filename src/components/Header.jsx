b583324a39d61520082737356f552954a0b0f6d6
davegreensmith <dave_greensmith@msn.com>
2022-07-20 10:23:16 +0100

    update with css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
+import logo from '../images/planet-logo.jpg'
+
 export default function () {
     return (
         <header id="header">
+            <img className="planet-logo" src={logo}></img>
             <h1>NC Planets</h1>
             <h2>To Infinity and Beyond</h2>
         </header>
